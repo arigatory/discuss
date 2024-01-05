aa1bc26e5eb9f4cbb40cf33aa3e56355afcdec8d
Ivan Panchenko <arigatory@gmail.com>
2024-01-05 05:59:37 +0300

    add paths helpers

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,28 @@
-import { Button } from '@nextui-org/react'
-import Image from 'next/image'
+import { Button } from "@nextui-org/react";
+import * as actions from "@/actions";
+import { auth } from "@/auth";
+import Profile from "@/components/profile";
 
-export default function Home() {
-  return <Button>Press me!!!</Button>
+export default async function Home() {
+  const session = await auth();
+
+  return (
+    <div>
+      <form action={actions.signIn}>
+        <Button type="submit">Sign in</Button>
+      </form>
+
+      <form action={actions.signOut}>
+        <Button type="submit">Sign out</Button>
+      </form>
+
+      {session?.user ? (
+        <div>{JSON.stringify(session.user)}</div>
+      ) : (
+        <div>Sidned Out</div>
+      )}
+
+      <Profile />
+    </div>
+  );
 }
