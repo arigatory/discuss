f30cf5f598d675faa838d54307a7c1d634ea14bd
Ivan Panchenko <arigatory@gmail.com>
2024-01-08 00:30:47 +0300

    add suspense

--- src/components/posts/post-show.tsx
+++ src/components/posts/post-show.tsx
@@ -6,6 +6,7 @@ interface PostShowProps {
 }
 
 export default async function PostShow({ postId }: PostShowProps) {
+  await new Promise(resolve => setTimeout(resolve, 2500))
   const post = await db.post.findFirst({
     where: { id: postId },
   });
@@ -13,7 +14,7 @@ export default async function PostShow({ postId }: PostShowProps) {
   if (!post) {
     notFound();
   }
-  
+
   return (
     <div className="m-4">
       <h1 className="text-2xl font-bold my-2">{post.title}</h1>
