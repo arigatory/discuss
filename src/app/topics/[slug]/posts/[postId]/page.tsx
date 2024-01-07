ad9bef04ed48019cbd24f9a903bc4dcd00dcf838
Ivan Panchenko <arigatory@gmail.com>
2024-01-08 00:08:33 +0300

    add recursive comments

--- src/app/topics/[slug]/posts/[postId]/page.tsx
+++ src/app/topics/[slug]/posts/[postId]/page.tsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import Link from "next/link";
+import PostShow from "@/components/posts/post-show";
+import CommentList from "@/components/comments/comment-list";
+import CommentCreateForm from "@/components/comments/comment-create-form";
+import paths from "@/paths";
+import { fetchCommentsByPostId } from "@/db/queries/comments";
 
-export default function PostShowPage() {
+interface PostShowPageProps {
+  params: {
+    slug: string;
+    postId: string;
+  };
+}
+
+export default async function PostShowPage({ params }: PostShowPageProps) {
+  const { slug, postId } = params;
+
   return (
-    <div>PostShowPage</div>
-  )
+    <div className="space-y-3">
+      <Link className="underline decoration-solid" href={paths.topicShow(slug)}>
+        {"< "}Back to {slug}
+      </Link>
+      <PostShow postId={postId} />
+      <CommentCreateForm postId={postId} startOpen />
+      <CommentList fetchData={() => fetchCommentsByPostId(postId)} />
+    </div>
+  );
 }
