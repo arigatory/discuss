cf4bbd3d8b86b0273bd0b885ea1f50ede946414e
Ivan Panchenko <arigatory@gmail.com>
2024-01-07 16:37:02 +0300

    add create post

--- src/app/topics/[slug]/page.tsx
+++ src/app/topics/[slug]/page.tsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import React from "react";
+import PostCreateForm from "@/components/posts/post-create-form";
 
-export default function TopicShowPage() {
+interface TopicShowPageProps {
+  params: {
+    slug: string;
+  };
+}
+
+export default function TopicShowPage({ params }: TopicShowPageProps) {
+  const { slug } = params;
+
   return (
-    <div>TopicShowPage</div>
-  )
+    <div className=" grid grid-cols-4 gap-4 p-4">
+      <div className=" col-span-3">
+        <h1 className=" text-2xl font-bold mb-2">{slug}</h1>
+      </div>
+
+      <div>
+        <PostCreateForm slug={slug}/>
+      </div>
+    </div>
+  );
 }
